3d4b9fab60b4c077d6ebd7e5239d1b797695083e
PujaGKundu <pujakundu100@gmail.com>
2022-01-01 11:53:14 +0530

    Assignment: Infanite Scroll

--- block-BJaaiq/code/index.js
+++ block-BJaaiq/code/index.js
@@ -1,3 +1,35 @@
-window.addEventListener('scroll', function() {
-  document.getElementById('showScroll').innerHTML = window.pageYOffset + 'px';
+let root = document.querySelector('ul');
+
+let max = 3;
+let index = 0;
+
+function addQuotes() {
+  for (; index < 3; index++) {
+    let li = document.createElement('li');
+    let blockquote = document.createElement('blockquote');
+    let cite = document.createElement('cite');
+    blockquote.innerText = quotes[index].quoteText;
+    cite.innerText = quotes[index].quoteAuthor;
+    li.append(blockquote, cite);
+    root.append(li);
+  }
+  index = 0
+}
+
+
+
+document.addEventListener('scroll', () => {
+  let scrollTop = document.documentElement.scrollTop;
+  let scrollHeight = document.documentElement.scrollHeight;
+  let clientHeight = document.documentElement.clientHeight;
+  if (scrollTop + clientHeight >= scrollHeight && index < quotes.length) {
+    addQuotes();
+  }
+});
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  alert(`The content of the DOM is loaded`);
+  addQuotes();
 });
